b2b6c3919133824566511847fdf25bb197c58d69
m8r1x <m8r1x.io@gmail.com>
2018-06-01 13:40:40 +0300

    Implement basic logic to pass test
    - Write tests
    - Write code using simple if/else statements to pass tests
    - Add support for conversion upto a million

---
+++ test/test.js
@@ -0,0 +1,17 @@
+import test from "ava";
+
+import nambari from "../lib";
+
+test("it correctly converts numbers < 10", t => {
+	t.is(nambari(1), "moja");
+});
+
+test("it correctly converts numbers < 20", t => {
+	t.is(nambari(13), "kumi na tatu");
+});
+
+test("it correctly converts numbers < 100", t => {
+	t.is(nambari(28), "ishirini na nane");
+	t.is(nambari(52), "hamsini na mbili");
+	t.is(nambari(90), "tisini");
+});
